c801c955b1085ec3312f1622cb955134498c4249
Anton Berger <aweo.wew@mail.ru>
2023-09-14 22:24:54 +0300

    create custom pipe and use ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface IUser {
+  name: string
+  age: number
+  id: number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EvoApp';
+
+  public date = new Date()
+  public number = 0;
+  public usersArray: IUser[] = [
+    { name: 'John', age: 19, id: 1 },
+    { name: 'Walter', age: 23, id: 2 },
+    { name: 'Victor', age: 25, id: 3 },
+    { name: 'Patric', age: 54, id: 4 },
+    { name: 'Werner', age: 12, id: 5 },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { AddFivePipe } from './pipes/add-five.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddFivePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
