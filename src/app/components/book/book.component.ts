827e1518e973f67817e0592d6817c8b06ada0005
Anton Berger <aweo.wew@mail.ru>
2023-09-20 19:46:25 +0300

    get data service

---
+++ src/app/components/book/book.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BooksService } from 'src/app/services/books.service';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+
+  constructor(public bookService: BooksService) { }
+
+
+}
