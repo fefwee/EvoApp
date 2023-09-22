09128fbf6037d6a4e825b82c84aa52f3ee308829
Anton Berger <aweo.wew@mail.ru>
2023-09-22 22:14:38 +0300

    add metatags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EvoApp';
+
+  constructor(private meta: Meta) {
+    this.meta.addTag({ property: "og:desc", content: "root_desc " })
+  }
 }
