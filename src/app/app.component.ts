22b44a288cdc123fe47b4fa9d4a016dba094dea2
Anton Berger <aweo.wew@mail.ru>
2023-09-28 00:02:13 +0300

    validation and regular expressions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+interface Book {
+  nameBook: string
+  auctor: string
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EvoApp';
+
+  public addNewBook: Book = {
+    nameBook: '',
+    auctor: ''
+  };
+
+  public books: Book[] = [
+    {
+      nameBook: 'Убить пересмешника',
+      auctor: 'Харпер Ли'
+    },
+    {
+      nameBook: 'Гордость и предубеждение',
+      auctor: 'Джейн Остен'
+    },
+    {
+      nameBook: 'Дневник Анны Франк',
+      auctor: 'Анна Франк'
+    }
+  ];
+
+
+  public addBook() {
+    this.books.push({
+      nameBook: this.addNewBook.nameBook,
+      auctor: this.addNewBook.auctor
+    })
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { MessagesModule } from 'primeng/messages';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule,
+    TableModule,
+    MessagesModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
